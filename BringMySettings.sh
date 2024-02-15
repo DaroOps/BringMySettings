4c503ad7b8d41ad58ee1126513422744811be8fb
daroOps <romerosd089@hotmail.com>
2024-02-14 19:50:08 -0500

    feat: :construction: Add function to simulate a progress or charge bar

--- BringMySettings.sh
+++ BringMySettings.sh
@@ -23,3 +23,23 @@ echo -e "
 " 
 }
 
+print_loading_bar() {
+    local bar_length=50
+    local total_duration=$1/2
+    local update_interval=$(awk "BEGIN { print $total_duration / $bar_length }")
+    
+    for ((i = 0; i <= bar_length; i++)); do
+        echo -ne "\r["
+        for ((j = 1; j <= bar_length; j++)); do
+            if [ "$j" -le "$i" ]; then
+                echo -ne "="
+            else
+                echo -ne "-"
+            fi
+        done
+        echo -ne "] $((i * 100 / bar_length))%"
+        sleep $update_interval
+    done
+
+    echo -ne "\n"
+}
