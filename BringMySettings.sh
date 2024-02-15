f4bcc4de7a980f8e91f86aa76af10f3912d1b814
daroOps <romerosd089@hotmail.com>
2024-02-14 20:18:00 -0500

    feat: :construction: Add function to check for the existence of gitconfig and overwrite it if necessary

--- BringMySettings.sh
+++ BringMySettings.sh
@@ -43,3 +43,40 @@ print_loading_bar() {
 
     echo -ne "\n"
 }
+
+replace_gitconfig(){
+    echo "No se encontró un archivo de configuración en $gitconfig_path."
+    touch ~/.gitconfig
+    echo "Se ha creado un nuevo archivo de configuración en $gitconfig_path."
+    echo -e "\n"
+
+    gitconfig_path=~/.gitconfig
+
+    if [ -f "$gitconfig_path" ]; then
+        echo "Se ha encontrado un archivo de configuración en $gitconfig_path."
+    else
+        echo "No se encontró un archivo de configuración en $gitconfig_path."
+        touch ~/.gitconfig
+        echo "Se ha creado un nuevo archivo de configuración en $gitconfig_path."
+    fi
+
+    existing_username=$(git config --global user.name)
+    existing_email=$(git config --global user.email)
+
+    if [ -n "$existing_username" ] && [ -n "$existing_email" ]; then
+        echo -e "\n"
+        echo "Configuraciones existentes:"
+        echo "Nombre de usuario: $existing_username"
+        echo "Correo electrónico: $existing_email"
+        echo -e "\n"
+    fi
+
+    read -p "Ingrese su nombre de usuario para Git: " new_username
+
+    read -p "Ingrese su correo electrónico para Git: " new_email
+
+    sed -i "s/\(\[user\]\s*\n\s*name\s*=\s*\).*\$/\1$new_username/" ~/.gitconfig
+    sed -i "s/\(\[user\]\s*\n\s*email\s*=\s*\).*\$/\1$new_email/" ~/.gitconfig
+
+    echo "Credenciales actualizadas con éxito."
+}
